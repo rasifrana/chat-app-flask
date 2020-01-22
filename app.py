a86c97466ca83d2f022424c76b8640b3aed490c3
Asif <muhammadasif@Asif.local>
2020-01-22 19:19:00 +0000

    new routes added for form to post data

--- app.py
+++ app.py
@@ -1,30 +1,36 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomkey123"
 messages = []
 
 
 def add_message(username, message):
     """Add messages to messages List"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
 
 
-def get_all_messages():
-    """Get all msgs and seperate with br tag"""
-    return "<br>".join(messages)
-
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instruction"""
-    return "To send a message use /USERNAME/MESSAGE"
+
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
+    return render_template("index.html")
 
 # route for username
 @app.route("/<username>")
 def user(username):
     """Display Chat Message"""
-    return "<h1>Welcome, {0} </h1> {1}".format(username, get_all_messages())
+    return "<h1>Welcome, {0} </h1> {1}".format(username, messages)
 
 # route for user chat
 @app.route("/<username>/<message>")
